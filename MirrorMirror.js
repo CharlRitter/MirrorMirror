a90c9690d2828e790058f2006b01ee1baa043b0c
CharlRitter <charlritter@hotmail.com>
2017-07-12 22:48:12 +0200

    Finished top 10 list
    Finished the list of top 10, with a header.
    Some bug seen that skips a portion of the actual top 10 candidates.
    Let the debugging commence.

--- MirrorMirror.js
+++ MirrorMirror.js
@@ -4,31 +4,45 @@ Start date:12 July 2017
 JourneyApps internship
 */
 
-
+//Retrieve all GitHub users from Cape Town with more than 1 follower.
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var xhrUsers = new XMLHttpRequest();
-xhrUsers.open("GET", "https://api.github.com/search/users?q=location:cape town followers:>=1", false);
+xhrUsers.open("GET", 'https://api.github.com/search/users?q=location:"Cape Town"+followers:>=10+language:Javascript', false);
 xhrUsers.setRequestHeader("Content-type", "application/json");
 xhrUsers.send();
 
-var output = JSON.parse(xhrUsers.responseText);
-var Login, FollowersUrl;
+var output = JSON.parse(xhrUsers.responseText);//Parse the output to JSON
+var login, url;
 var numFollowers = 0;
+var userArr = [];
 
+//Loop through every GitHub user.
 for (i in output.items) { 
-    for (j in output.items[i].login) {
-        Login = output.items[i].login;
-        FollowersUrl =  output.items[i].followers_url;
-        }
+    login = output.items[i].login;
+    url =  output.items[i].url;
+
+    //Retrieve all followers from the GitHub user.
     var xhrFollowers = new XMLHttpRequest();
-    xhrFollowers.open("GET", FollowersUrl, false);
+    xhrFollowers.open("GET", url, false);
     xhrFollowers.setRequestHeader("Content-type", "application/json");
     xhrFollowers.send();
     var temp = JSON.parse(xhrFollowers.responseText);
-    numFollowers = temp.length;
 
-    console.log(Login);
-    console.log(numFollowers);
-    console.log();
+    numFollowers = temp.followers;
+    //An if statement to add a zero infront of any number of followers less than ten, to make the sorting of the array work.
+    if(numFollowers<10){
+        numFollowers = "0"+numFollowers;
+    }
+    userArr.push(numFollowers+" "+login); //add number of followers and username to an array.
     numFollowers = 0;
+}
+
+userArr.sort();//Sorts the elements of the array.
+
+userArr.reverse();//Reverses the order of the elements.
+
+//Print out a heading and the 10 most popular javascript developers in Cape Town.
+console.log("The 10 most popular javascript developers in Cape Town are:");
+for(i=0; i<10; i++){
+    console.log(i+1 +") "+ userArr[i]);
 }
--- ReadMe.txt
+++ ReadMe.txt
@@ -1,4 +1,9 @@
 Charl Ritter
-Start date:12 July 2017
+Start date: 12 July 2017
+Finish date: 12 July 2017
 JourneyApps internship - MirrorMirror
 
+Instructions:
+1) Open Command Prompt.
+2) Navigate to the directory that contains the MirrorMirror.js file (../MirrorMirror).
+3) Run the command "node MirrorMirror".
