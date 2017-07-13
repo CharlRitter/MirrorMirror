acaae0ed5db8b15841c29313165dc9b784ce32c7
CharlRitter <CharlRitter@users.noreply.github.com>
2017-07-13 15:51:58 +0200

    Finished project
    I have finished the project. I made aesthetic changes, finished comments since last commit and updated the ReadMe.txt file.

--- MirrorMirror.js
+++ MirrorMirror.js
@@ -4,60 +4,77 @@ Start date:12 July 2017
 JourneyApps internship
 */
 
-//Retrieve all GitHub users from Cape Town with more than 1 follower.
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var xhrUsers = new XMLHttpRequest();
-xhrUsers.open("GET", 'https://api.github.com/search/users?q=location:"Cape Town"+followers:>=10+language:Javascript', false);
-xhrUsers.setRequestHeader("Content-type", "application/json");
-xhrUsers.send();
+//Creates a localhost that displayes the top 10 list.
+var http = require('http');
 
-var output = JSON.parse(xhrUsers.responseText);//Parse the output to JSON
-var login, url;
-var numFollowers = 0;
-var userArr = [];
-
-//Loop through every GitHub user.
-for (i in output.items) { 
-    login = output.items[i].login;
-    url =  output.items[i].url;
-
-    //Retrieve all followers from the GitHub user.
-    var xhrFollowers = new XMLHttpRequest();
-    xhrFollowers.open("GET", url, false);
-    xhrFollowers.setRequestHeader("Content-type", "application/json");
-    xhrFollowers.send();
-    var temp = JSON.parse(xhrFollowers.responseText);
-
-    numFollowers = temp.followers;
-    //If statements to add zeros infront of any number of followers, to make the sorting of the array work.
-    if(numFollowers<10){
-        numFollowers = "00"+numFollowers;
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var Arr = [];
+    var txt1 = "The 10 most popular javascript developers in Cape Town are: ";
+    var txt2;
+    Arr = getUsers();
+    //Print out a heading and the 10 most popular javascript developers in Cape Town.
+    res.write("<p>" + txt1.fontsize(5).bold() + "</p>" + "<br>");
+    for(k=0; k<10; k++){
+        txt2 = k+1 + ") " + Arr[k];
+        res.write(txt2.fontsize(4) + "<br>");
     }
-    else if(numFollowers<100 && numFollowers>=10){
-        numFollowers = "0"+numFollowers;
-    }
-    userArr.push(numFollowers+" "+login); //add number of followers and username to an array.
-    numFollowers = 0;
-}
+    res.end();
+}).listen(8080);
 
-userArr.sort(); //Sorts the elements of the array.
-userArr.reverse(); //Reverses the order of the elements.
+//A function to retrieve an array of usernames & their followers.
+function getUsers(){
+    //Retrieve all GitHub users from Cape Town with more than 1 follower.
+    var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+    var xhrUsers = new XMLHttpRequest();
+    xhrUsers.open("GET", 'https://api.github.com/search/users?q=location:"Cape Town"+followers:>=10+language:Javascript', false);
+    xhrUsers.setRequestHeader("Content-type", "application/json");
+    xhrUsers.send();
 
-var flag = 0;
-for(i in userArr){
-    while(flag == 0){
-        if(userArr[i].charAt(0) != 0){
-            flag = 1;
+    var output = JSON.parse(xhrUsers.responseText);//Parse the output to JSON
+    var login, url;
+    var numFollowers = 0;
+    var userArr = [];
+
+    //Loop through every GitHub user.
+    for (i in output.items) { 
+        login = output.items[i].login;
+        url =  output.items[i].url;
+
+        //Retrieve all followers from the GitHub user.
+        var xhrFollowers = new XMLHttpRequest();
+        xhrFollowers.open("GET", url, false);
+        xhrFollowers.setRequestHeader("Content-type", "application/json");
+        xhrFollowers.send();
+        var temp = JSON.parse(xhrFollowers.responseText);
+
+        numFollowers = temp.followers;
+        //If statements to add zeros infront of any number of followers, to make the sorting of the array work.
+        if(numFollowers<10){
+            numFollowers = "00"+numFollowers;
         }
-        else{
-            userArr[i] = userArr[i].substr(1, userArr[i].length);
+        else if(numFollowers<100 && numFollowers>=10){
+            numFollowers = "0"+numFollowers;
         }
+        userArr.push(numFollowers+" "+login); //add number of followers and username to an array.
+        numFollowers = 0;
     }
-    flag = 0;
-}
 
-//Print out a heading and the 10 most popular javascript developers in Cape Town.
-console.log("The 10 most popular javascript developers in Cape Town are:");
-for(k=0; k<10; k++){
-    console.log(k+1 +") "+ userArr[k]);
+    userArr.sort(); //Sorts the elements of the array.
+    userArr.reverse(); //Reverses the order of the elements.
+
+    var flag = 0;
+    //To remove the zeros added earlier, infront of number of followers, to make sorting easier.
+    for(i in userArr){
+        while(flag == 0){
+            if(userArr[i].charAt(0) != 0){
+                flag = 1;
+            }
+            else{
+                userArr[i] = userArr[i].substr(1, userArr[i].length);
+            }
+        }
+        flag = 0;
+    }
+    return userArr;
 }
--- ReadMe.txt
+++ ReadMe.txt
@@ -4,6 +4,9 @@ Finish date: 13 July 2017
 JourneyApps internship - MirrorMirror
 
 Instructions:
-1) Open Command Prompt.
-2) Navigate to the directory that contains the MirrorMirror.js file (../MirrorMirror).
-3) Run the command "node MirrorMirror".
+1) Download files from GitHub.
+2) Open Command Prompt.
+3) Navigate to the directory that contains the MirrorMirror.js file (../MirrorMirror OR ../MirrorMirror-master).
+4) Run the command "node MirrorMirror".
+5) Open a browser and go to http://localhost:8080/
+6) The top 10 list will be displayed here.
